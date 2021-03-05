c6ca7cc5719c944a4ea6002f85596c763df69279
Ahlad Reddy <ahlad.reddy@network.rca.ac.uk>
2021-03-04 16:13:21 -0800

    implemented GET

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-from flask import Flask 
+from flask import Flask, request, jsonify
+import os
+from pwd import getpwuid
+
+ROOT_DIR = 'data'
+
 app = Flask(__name__)
 
 @app.route('/')
-def api():
-  return 'Hello World'
+@app.route('/<path:subpath>')
+def api(subpath="", methods=['GET']):
+  if request.method == 'GET':
+    path = os.path.join(ROOT_DIR, subpath)
+    if os.path.isdir(path):
+      files = []
+      dirs = []
+      for f in os.listdir(path):
+        fp = os.path.join(path, f)
+        if os.path.isfile(fp):
+          stats = os.stat(fp)
+          files.append({ 
+            'name': f, 
+            'owner': getpwuid(stats.st_uid).pw_name, 
+            'size': stats.st_size, 
+            'permissions': oct(stats.st_mode)[-3:],
+          })
+        else:
+          dirs.append(f)
+      return jsonify({ 'files': files, 'directories': dirs })
+    elif os.path.isfile(path):
+      file = open(path, 'r')
+      return jsonify({ 'content': file.read() })
+    else:
+      return jsonify({ 'error': 'path does not exist'})
