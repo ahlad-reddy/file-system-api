34f6b0c3c947824d6b21d4eb9db7b8995aab98cc
Ahlad Reddy <ahlad.reddy@network.rca.ac.uk>
2021-03-05 18:48:02 -0800

    coverage tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 export ROOT_DIR=./example_dir
-docker-compose run web pytest
+docker-compose up -d
+docker-compose exec web coverage run -m pytest
+docker-compose exec web coverage report
+docker-compose down
